6bd37c6b25d14fb2c39ff60f6f1853b08034951c
professor-wanderson-timoteo <wanderson_timoteo@outlook.com>
2024-08-07 11:27:05 -0400

    add animation to switch and script js

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  // Pegar a tag img
+  const img = document.querySelector("#profile img");
+
+  // Substituir a imagem
+  if(html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png");
+  } else {
+    // Se tiver sem light mode, manter a imagem atual
+    img.setAttribute("src", "./assets/avatar.png");
+  }
+}
